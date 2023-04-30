fe58082bf1de74a621966e9211e0e7d02fc19742
animeshjain7 <108515181+animeshjain7@users.noreply.github.com>
2023-04-30 09:13:38 +0000

    css of form completed

--- src/Todo App/TodoItem.js
+++ src/Todo App/TodoItem.js
@@ -1,15 +1,26 @@
-export default function TodoItem({completed, id, title, toggleTodo, deleteTodo}) {
+import "./style.css";
+
+export default function TodoItem({
+  completed,
+  id,
+  title,
+  toggleTodo,
+  deleteTodo,
+}) {
   return (
     <li>
       <label>
         <input
+          className="checkbox"
           type="checkbox"
           checked={completed}
           onChange={(e) => toggleTodo(id, e.target.checked)}
         />
         {title}
       </label>
-      <button onClick={() => deleteTodo(id)}>Delete</button>
+      <button className="button-delete" onClick={() => deleteTodo(id)}>
+        Delete
+      </button>
     </li>
   );
 }
