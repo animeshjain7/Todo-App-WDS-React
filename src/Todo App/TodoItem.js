915cfef3883b8e454001797711cb5c6aa60b8b45
animeshjain7 <108515181+animeshjain7@users.noreply.github.com>
2023-04-30 10:37:56 +0000

    list update 1

--- src/Todo App/TodoItem.js
+++ src/Todo App/TodoItem.js
@@ -8,15 +8,16 @@ export default function TodoItem({
   deleteTodo,
 }) {
   return (
-    <li>
-      <label>
-        <input
-          className="checkbox"
-          type="checkbox"
-          checked={completed}
-          onChange={(e) => toggleTodo(id, e.target.checked)}
-        />
-        {title}
+    <li className="li-tag">
+      <input
+        id={id}
+        className="checkbox-input"
+        type="checkbox"
+        checked={completed}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
+      <label htmlFor={id}>
+        <div className="content-list">{title}</div>
       </label>
       <button className="button-delete" onClick={() => deleteTodo(id)}>
         Delete
